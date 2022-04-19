c0e8fb9e13ba5ee8cff1ef358d06d45acd190ec1
RTemiy <83100656+RTemiy@users.noreply.github.com>
2022-04-19 18:19:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const Entities = [
     }),
 ];
 
-var IntroMusic = new Audio(https://rtemiy.github.io/bloody_trail/Intro.mp3);
+var IntroMusic = new Audio("https://rtemiy.github.io/bloody_trail/Intro.mp3");
 
 //STORYLINE
 
